80928025c17ab303e2bfe9331ff2b52f8bf3c5bd
Johnny Sebastian <jsp@Johnnys-MacBook-Air.local>
2019-12-09 22:53:57 -0800

    change in dist folder and publicpath to fix githubpages

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'this-year-progress',
+    assetsPublicPath: '/this-year-progress/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: '/this-year-progress/',
+  publicPath: '/this-year-progress',
 };
